8d6471b3d46da7a6871372b00082ad109bcb4c46
tamaki8021 <hbk2002320@gmail.com>
2022-11-21 15:58:57 +0900

    feat: hammerjsのインストール

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import * as bootstrap from "bootstrap";
+import "bootstrap";
 import "@fortawesome/fontawesome-free";
+import 'hammerjs';
+
 import './custom/profile_image_upload';
