224b1f21ce92f9f8c089731a96642a0b3ed16508
tamaki8021 <hbk2002320@gmail.com>
2022-12-03 14:59:55 +0900

    feat: chat messageを保存

--- app/channels/chat_room_channel.rb
+++ app/channels/chat_room_channel.rb
@@ -8,6 +8,10 @@ class ChatRoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    ActionCable.server.broadcast('chat_room_channel', { chat_message: data['chat_message'] })
+    ChatMessage.create!(
+      content: data['chat_message'],
+      user_id: current_user.id,
+      chat_room_id: data['chat_room_id']
+    )
   end
 end
--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -14,16 +14,17 @@ const appRoom = consumer.subscriptions.create("ChatRoomChannel", {
     return alert(data["chat_message"]);
   },
 
-  speak: function (chat_message) {
-    return this.perform("speak", { chat_message: chat_message });
-  },
+  speak: function(chat_message, chat_room_id) {
+    return this.perform('speak', { chat_message: chat_message, chat_room_id: chat_room_id });
+  }
 });
 
 if (/chat_rooms/.test(location.pathname)) {
   window.document.addEventListener("keydown", (event) => {
     if (event.target.className === "chat-room__message-form_textarea") {
       if (event.key == "Enter") {
-        appRoom.speak(event.target.value);
+        const chat_room_id = document.querySelector('textarea').dataset.chat_room_id;
+        appRoom.speak(event.target.value, chat_room_id);
         event.target.value = "";
         event.preventDefault();
       }
